3fbe2ce5c97302ec6d54d01386d8d0f824622dec
Felix <fastfelix771@gmail.com>
2016-08-04 00:34:53 +0200

    Added current usage of the ProtocolLib API to v1_7 and above vSign Handlers. v1_6 and lower uses the deprecated, legacy API to provide support for the old versions of ProtocolLib.

--- src/main/java/de/unitygaming/bukkit/vsign/packets/v1_7/ProtocolLibvSign.java
+++ src/main/java/de/unitygaming/bukkit/vsign/packets/v1_7/ProtocolLibvSign.java
@@ -1,6 +1,7 @@
 package de.unitygaming.bukkit.vsign.packets.v1_7;
 
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
@@ -9,6 +10,7 @@ import com.comphenix.protocol.events.PacketContainer;
 import de.unitygaming.bukkit.vsign.Version;
 import de.unitygaming.bukkit.vsign.VirtualSign;
 import de.unitygaming.bukkit.vsign.invoker.Invoker;
+import de.unitygaming.bukkit.vsign.invoker.ReturningInvoker;
 
 public class ProtocolLibvSign extends de.unitygaming.bukkit.vsign.packets.v1_6.ProtocolLibvSign implements VirtualSign {
 
@@ -25,6 +27,21 @@ public class ProtocolLibvSign extends de.unitygaming.bukkit.vsign.packets.v1_6.P
 		.write(2, 0);
 
 		Version.getCurrent().getPacketHandler().sendPacket(player, packet);
+	}
+	
+	@Override
+	public void setup(Plugin plugin, final Player player) {
+
+		Version.getCurrent().getPacketHandler().addPacketListener(plugin, player, PacketType.Play.Client.UPDATE_SIGN, new ReturningInvoker<PacketContainer, Boolean>() {
+
+			@Override
+			public Boolean invoke(PacketContainer packet) {
+				if(!pending.containsKey(player.getName())) return false;
+				pending.remove(player.getName()).invoke(packet.getStringArrays().read(0));
+				return true;
+			}
+		}, true);
+
 	}
 
 }
